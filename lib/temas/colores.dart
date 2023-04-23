242768e8ad542e8602f06c107cea0dc2aac6b372
LeslieRodas <123709847+LeslieRodas@users.noreply.github.com>
2023-04-22 22:46:36 -0600

    Add files via upload

---
+++ lib/temas/colores.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const Color bgColor = Colors.black;
+const Color blanco = Colors.white;
+const Color primario = Color(0xFF399df8);
+const Color grisColor = Colors.grey;
+const Color cajaChatOtros=Color(0xFF3d3d3f);
+const Color cajasChat=Color(0xFF066162);
